3c3bdccb5012e4cd12861c69dc1a790a9dbc2f4d
Sayem Hoque <sayemmh@gmail.com>
2019-01-01 16:28:30 -0500

    messing around w levels

--- levels.py
+++ levels.py
@@ -8,14 +8,53 @@ import time
 #
 ###
 
-levels = []
-for num in range(1, 1665):
-    with open('word/' + str(num) + '.pckl', 'rb') as fp:
-         l = pickle.load(fp)
-    levels.append([num, len(l[0])])
+def open_word(key):
+    with open('words/' + str(key) + '.pckl', 'rb') as fp:
+        l = pickle.load(fp)
+    return l
 
-with open("levels.pckl", "wb") as fp:
-    pickle.dump(levels, fp)
+# counts = []
+# for num in range(1, 1665):
+#     with open('words/' + str(num) + '.pckl', 'rb') as fp:
+#          l = pickle.load(fp)
+#     counts.append([num, len(l[0])])
+# counts.sort(key=lambda x:x[1], reverse=True)
+# with open("counts.pckl", "wb") as fp:
+#     pickle.dump(counts, fp)
 
-total_words = [sum(x) for x in zip(*levels)][1]
-print(total_words)
+# with open("counts.pckl", "rb") as fp:
+#     counts = pickle.load(fp)
+
+# total word count
+total_word_count = [sum(x) for x in zip(*counts)][1]
+print(total_word_count)
+
+# top 5 root words in corpus
+counts[:5]
+# >>> [[61, 2851], [1245, 1722], [59, 1464], [1305, 1390], [524, 980]]
+w1 = open_word(61)[1]
+w1
+# ['your God',
+#  'and (the) God',
+#  'God',
+#  'And your God',...]
+w2 = open_word(1245)[1]
+w2
+# ['say',
+#  'it is said',
+#  'they say',
+#  'it is said',
+#  'they say',
+#  'they say',
+#  'they say',
+w3 = open_word(59)[1]
+w3
+# ['(of) those',
+#  'Those who',
+#  'And those who',
+#  'those who',
+#  'and those who',
+#  'those who',
+#  '(are) the ones who',
+w4 = open_word(1305)[1]
+w4
