23845f47d6d06dbf34f74a12721796f05c43bbeb
Sayem Hoque <sayemmh@gmail.com>
2020-01-23 00:45:48 -0500

    added comments for functions

--- wordsFromSurah.py
+++ wordsFromSurah.py
@@ -3,27 +3,42 @@ import pickle
 import time
 import pandas as pd
 
-###
-# out is a 2-d list where each inner list is a key, # of occurrences pair for each arabic root wordx
-#
-###
+def open_rootword(id):
+    '''
+    Open the DataFrame associated with a particular root word. The `id`s
+    go from 1 to 1664 representing the 1664 root words in the corpus.
 
-def open_word(key):
-    with open('pckl-words/' + str(key) + '.pckl', 'rb') as fp:
+    :param int id: the id of the rootword to open
+    :return: DataFrame containing all words in this root words' group
+    :rtype: pandas.core.frame.DataFrame
+    '''
+    with open('pckl-words/' + str(id) + '.pckl', 'rb') as fp:
         l = pickle.load(fp)
-        # print(l)
     return l
 
 def getSurah(surahNumber, ayahStart, ayahEnd, minCount, maxCount):
+    '''
+    Get words within a surah starting from one ayah to another. Optional - 
+    get words with a frequency above `minCount` and lower than `maxCount`.
+
+    :param int surahNumber: which surah to get
+    :param int ayahStart: which ayah to start at
+    :param int ayahEnd: which ayah to end at
+    :param int minCount: min frequency of a word to return
+    :param int maxCount: max frequency of a word to return
+    :return: 
+    :rtype: 
+    '''
+
+    ayahStart += 1
+    ayahEnd += 1
+    
     count = 0
     for num in range(1, 1665):
-        w = open_word(num)
+        w = open_rootword(num)
         if (len(w) <= minCount or len(w) > maxCount):
             continue
         wm = w['wordmorphologies']
-
-        w2 = []
-
 
         for i in range(0, len(wm)):
             if (int(wm[i].split('(')[1].split(':')[0]) == surahNumber and int(wm[i].split('(')[1].split(':')[1]) >= ayahStart and int(wm[i].split('(')[1].split(':')[1]) < ayahEnd):
@@ -35,17 +50,17 @@ def getSurah(surahNumber, ayahStart, ayahEnd, minCount, maxCount):
                 tlit = w['pronunciations'][i]
                 ayah = ",".join(wm[i].split('(')[1].split(':')[0:2])
                 answer = ' '.join(answer.split('\''))
-                print('{',
-                        'question:'+ '\''+ question +'\''+ ','+
-                        'answer:'+ '\''+ answer +'\''+ ','+
-                        'pcklId:'+ str(i) + ','+
-                        'tlit:'+ '\''+ tlit +'\''+ ',',
-                        'num:'+ '\''+ str(len(w)) +'\''+ ',',
-                        'ayah:'+ '\''+ ayah +'\''+ ',',
-                        '}'+ ',')
+                # print('{',
+                #         'question:'+ '\''+ question +'\''+ ','+
+                #         'answer:'+ '\''+ answer +'\''+ ','+
+                #         'pcklId:'+ str(i) + ','+
+                #         'tlit:'+ '\''+ tlit +'\''+ ',',
+                #         'num:'+ '\''+ str(len(w)) +'\''+ ',',
+                #         'ayah:'+ '\''+ ayah +'\''+ ',',
+                #         '}'+ ',')
 
 
-                # print('\''+ answer +'\''+',')
+                print('\''+ answer +'\''+',')
 
 if __name__ == '__main__':
-    getSurah(1, 0, 8, 0, 100000)
+    getSurah(1, 1, 7, 0, 100000)
